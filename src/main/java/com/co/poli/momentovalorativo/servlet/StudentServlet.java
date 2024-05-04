ce4100d65fc5abc568c7ced87d309fbc36acc129
fabith95 <fabith95@gmail.com>
2024-05-04 17:31:51 -0500

    Create GSON JSON and conexion servlet for browser with db

--- src/main/java/com/co/poli/momentovalorativo/servlet/StudentServlet.java
+++ src/main/java/com/co/poli/momentovalorativo/servlet/StudentServlet.java
@@ -2,6 +2,8 @@ package com.co.poli.momentovalorativo.servlet;
 
 import com.co.poli.momentovalorativo.database.ConexionMySql;
 import com.co.poli.momentovalorativo.model.Students;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,6 +17,13 @@ import java.util.ArrayList;
 @WebServlet(name = "studentServlet", value = "/students")
 public class StudentServlet extends HttpServlet {
     private ConexionMySql con;
+    private GsonBuilder gsonBuilder;
+    private Gson gson;
+    public void init() {
+        con = new ConexionMySql();
+        gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
@@ -22,7 +31,7 @@ public class StudentServlet extends HttpServlet {
             if(request.getParameter("id") == null) {
                 ArrayList<Students> listStudent = (ArrayList<Students>) con.getStudents();
                 PrintWriter out = response.getWriter();
-                //out.print(gson.toJson(listStudent));
+                out.print(gson.toJson(listStudent));
                 out.flush();
             }else{
 
@@ -39,7 +48,7 @@ public class StudentServlet extends HttpServlet {
             if(request.getParameter("id") == null) {
                 ArrayList<Students> listAllStudent = (ArrayList<Students>) con.getStudentInsert();
                 PrintWriter out = response.getWriter();
-                //out.print(gson.toJson(listAllStudent));
+                out.print(gson.toJson(listAllStudent));
                 out.flush();
             }else{
 
